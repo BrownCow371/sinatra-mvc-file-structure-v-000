9b486cc057d6a55edc48a2d3b2549defd587d947
Jamie Brown <jmbrowncow@gmail.com>
2018-08-07 21:31:32 +0000

    automatically backed up by learn

---
+++ app/models/dog.rb
@@ -0,0 +1,12 @@
+class Dog
+  attr_accessor :name, :breed, :age
+
+  @@all=[]
+
+  def initialize (name, breed, age)
+    self.name = name
+    self.breed = breed
+    self.age= age
+    @@all<<self
+  end
+end
